9c1ddc52a593d8e62527c44a9489221f3fd163f5
Lewis Holgate <lholgate6162@gmail.com>
2021-10-20 18:48:34 -0600

    moved javascript into functions and set up change of month

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,26 +1,33 @@
 var calendarData = [];
+var currentDay = {};
 var currentYear = 2021;
 var currentMonth = 5; //June
+var calendarDay = document.getElementsByClassName("days");
+var  selectedMonth = document.getElementById("start");
+console.log(selectedMonth.value);
 
-// calendarData.push("February");
+let loadArray = function(){
+    calendarData = [];
+    let selected = selectedMonth.value.split('-');
+    currentYear = selected[0];
+    currentMonth = selected[1];
+    console.log(currentMonth);
 
-let lastDay = new Date(currentYear,currentMonth,0).getDate();
-console.log(lastDay);
+    let lastDay = new Date(currentYear,currentMonth+1,0).getDate();
+    console.log(lastDay);
 
-for (let i = 1; i <= lastDay;i++){
-    var d = new Date(currentYear,currentMonth,i);
-    var date = d.getDate();
-    var day = d.getDay();
+    for (let i = 1; i <= lastDay;i++){
+        var d = new Date(currentYear,currentMonth-1,i);
+        var date = d.getDate();
+        var day = d.getDay();
 
-    var weekOfMonth = Math.ceil((date - 1 - day) / 7)+1;
+        var weekOfMonth = Math.ceil((date - 1 - day) / 7)+1;
+        console.log(d+":"+date+":"+day+":"+weekOfMonth+":"+currentMonth);
+        calendarData.push({day:i,dayOfWeek:day,weekOfMonth:weekOfMonth,image:'image name',brightness:.1});
 
-    calendarData.push({day:i,dayOfWeek:day,weekOfMonth:weekOfMonth,image:'image name',brightness:.1});
+    };
+}
 
-};
-
-console.log(calendarData);
-
-var currentDay = {};
 let getData = function(dOW,wOM){
 
     calendarData.forEach(function(entry) {
@@ -31,14 +38,17 @@ let getData = function(dOW,wOM){
 });
 }
 
-var calendarDay = document.getElementsByClassName("days");
-var i;
-for (i = 0; i < calendarDay.length; i++) {
-    getData(calendarDay[i].dataset.dow, calendarDay[i].dataset.wom);
+let loadCalendar = function() {
+    for (let i = 0; i < calendarDay.length; i++) {
+        getData(calendarDay[i].dataset.dow, calendarDay[i].dataset.wom);
 
-    if(currentDay){
-        calendarDay[i].textContent=currentDay.day;
-        
+        if(currentDay){
+            calendarDay[i].textContent=currentDay.day;
+            
+        }
+        currentDay = {};
     }
-    currentDay = {};
 }
+
+loadArray();
+loadCalendar();
